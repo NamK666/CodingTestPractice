48b3dd65d621d1d6434f70c5b9b306ee0b26bc09
NamK666 <chriscole77@naver.com>
2024-06-10 17:19:44 +0900

    codingtest1

--- src/practice3.java
+++ src/practice3.java
@@ -165,22 +165,5 @@ public class practice3 {
         return result;
     }
     */
-    static String starSideDel(String str) {
-        String result = "";
 
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) != '*') {
-                result += String.valueOf(str.charAt(i));
-            }
 
-            if (str.charAt(i) == '*' && str.charAt(i - 1) != '*') {
-                result = result.substring(0, result.length()-1);
-            }
-        }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(starSideDel("cd*zq"));
-    }
-}
